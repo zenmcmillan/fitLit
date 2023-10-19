32c5363dd8b8bc81d3c31a58f42fc44aa6f3f8e7
Zen McMillan <zenmcmillan@gmail.com>
2023-10-18 20:09:43 -0600

    Create averageHoursSleptPerDay which will return the average hours slept per day for a particular user

--- test/sleep-functions.js
+++ test/sleep-functions.js
@@ -1,2 +1,12 @@
+export const averageHoursSleptPerDay = (array, id) => {
+  let total = 0;
+  const hours = array
+    .filter((person) => person.userID === id)
+    .map((person1) => person1.hoursSlept);
 
+  hours.forEach((dailyHours) => {
+    total += dailyHours;
+  });
 
+  return total / hours.length;
+};

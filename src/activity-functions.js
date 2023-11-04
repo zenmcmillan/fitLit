621bc333262efcaa1d594ca32e745db0198f8977
Joseph Stewart <JosephStewart1752@gmail.com>
2023-11-04 12:39:14 -0400

    Remove comments

--- src/activity-functions.js
+++ src/activity-functions.js
@@ -28,34 +28,6 @@ export const didMeetStepGoal = (array1, array2, id, date) =>{
   }
 }
 
-// export const addNewActivity = userActivities => {
-//   activityContent.innerHTML += `
-//       <p>UserID: ${userActivities.userID}</p>
-//       <p>Date: ${userActivities.date}</p>
-//       <p>Number of Steps: ${userActivities.numSteps}</p>
-//       <p>Minutes Active: ${userActivities.minutesActive}</p>
-//       <p>Flights of Stairs: ${userActivities.flightsOfStairs}</p>
-//       `
-// }
 
-// const form = document.querySelector('form hidden')
-
-// form.addEventListener('.submit-button', (e) => {
-//   e.preventDefault();
-//   const formData = new FormData(e.target);
-//   const newActivityData = {
-//     userID: formData.get('user id'),
-//     date: formData.get('date'),
-//     numSteps: formData.get('number-of-steps'),
-//     minutesActive: formData.get('minutes-active'),
-//     flightsOfStairs: formData.get('flights-of-stairs')
-//   };
-//   if (!newActivityData.date || !newActivityData.numSteps|| !newActivityData.minutesActive || !newActivityData.flightsOfStairs || !newActivityData.userID){
-//     alert("You need to fill all fields before proceeding!")
-//     return
-//   }
-//   addNewActivity(newActivityData);
-//   e.target.reset();
-// });
 
 
